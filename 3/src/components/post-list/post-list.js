83cd76e9a943c8c96e06be1413b2b646f6ddd6d5
Max <maxim.diachuk@yahoo.com>
2019-06-12 02:07:23 +0300

    lesson 4

--- 3/src/components/post-list/post-list.js
+++ 3/src/components/post-list/post-list.js
@@ -3,12 +3,22 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css'
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    let elements = posts.filter( item => typeof item === 'object' );
+
+    elements = elements.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key={id} className='list-group-item'>
+                <PostListItem {...itemProps} />    
+            </li>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
